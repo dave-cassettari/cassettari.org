8e3aece795b49e982d90b7f45b40474fddc38582
dave-cassettari <davec1190+github@gmail.com>
2013-11-26 17:53:28 +0000

    Simplified portfolio
    Removed old files

--- index.php
+++ index.php
@@ -1,15 +1,3 @@
-<?php
-
-//include('functions.php');
-
-$files = glob('images/resized/*.jpg');
-
-shuffle($files);
-
-$index = array_rand($files);
-$file = $files[$index];
-
-?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -21,35 +9,81 @@ $file = $files[$index];
 	<meta name='viewport' content='width=device-width, initial-scale=1, user-scalable=no'>
 
 	<link rel='stylesheet' type='text/css' href='//fonts.googleapis.com/css?family=Open+Sans:300'/>
-	<link rel='stylesheet' type='text/css' href='/css/compiled/v3.css'/>
+	<link rel='stylesheet' type='text/css' href='/css/compiled/style.css'/>
 </head>
 <body>
 
 <h1 class='title'>cassettari.org</h1>
 
-<div class='backgrounds'>
-	<img src='<?php echo $file; ?>'/>
-</div>
+<ul class='projects'>
+	<li class='project'>
+		<div class='stack'>
+			<div class='progress'>
+				<div class='progress-complete progress-complete-full'></div>
+			</div>
 
-<div class='projects'>
-	<a class='project' target='_blank' href='http://www.102labs.com/'>
-		<img class='project-image' src='/images/projects/102.jpg'/>
-		<span class='project-name'>102labs</span>
-	</a>
+			<span class='stack-client'>AngularJS</span>
+			<span class='stack-server'>C# ASP.NET MVC</span>
+			<span class='stack-database'>Hadoop + HBase</span>
+		</div>
 
-	<a class='project' target='_blank' href='https://www.investinme.co.uk/'>
-		<img class='project-image' src='/images/projects/iim.jpg'/>
-		<span class='project-name'>Invest In Me</span>
-	</a>
+		<a class='project-link' target='_blank' href='http://www.102labs.com/'>
+			<img class='project-image' src='/images/projects/102.jpg'/>
+			<span class='project-name'>102labs</span>
+		</a>
+	</li>
 
-	<a class='project' target='_blank' href='https://www.db.com/index_e.htm'>
-		<img class='project-image' src='/images/projects/db.jpg'/>
-		<span class='project-name'>Deutsche Bank</span>
-	</a>
-</div>
+	<li class='project'>
+		<div class='stack'>
+			<div class='progress'>
+				<div class='progress-complete progress-complete-full'></div>
+			</div>
 
-<script src='/js/jquery.js'></script>
-<script src='/js/imagesloaded.js'></script>
-<script src='/js/tiles.js'></script>
+			<span class='stack-client'>jQuery, </span>
+			<span class='stack-server'>PHP + Yii</span>
+			<span class='stack-database'>MySQL</span>
+		</div>
+
+		<a class='project-link' target='_blank' href='https://www.investinme.co.uk/'>
+			<img class='project-image' src='/images/projects/iim.jpg'/>
+			<span class='project-name'>Invest In Me</span>
+		</a>
+	</li>
+
+	<li class='project'>
+		<div class='stack'>
+			<div class='progress'>
+				<div class='progress-complete progress-complete-full'></div>
+			</div>
+
+			<span class='stack-client'>HTML5</span>
+			<span class='stack-server'>PHP + Yii</span>
+			<span class='stack-database'>PostgreSQL</span>
+		</div>
+
+		<a class='project-link' target='_blank' href='http://www.aviationlist.co.uk'>
+			<img class='project-image' src='/images/projects/rfp.jpg'/>
+			<span class='project-name'>Aviation List</span>
+		</a>
+	</li>
+
+	<li class='project'>
+		<div class='stack'>
+			<div class='progress'>
+				<div class='progress-complete progress-complete-half'></div>
+			</div>
+
+			<span class='stack-client'>HTML5</span>
+			<span class='stack-server'>PHP + Concrete5</span>
+			<span class='stack-database'>MySQL</span>
+		</div>
+
+		<a class='project-link' target='_blank' href='http://www.geoinformationgroup.co.uk/'>
+			<img class='project-image' src='/images/projects/tgg.jpg'/>
+			<span class='project-name'>The GeoInformation Group</span>
+		</a>
+	</li>
+</ul>
+
 </body>
 </html>
